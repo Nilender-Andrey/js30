b9be89ad2dbac355f7cb872a3f16e93ed2209361
Andrey Nilender <nilenderav@mail.ru>
2024-09-19 20:51:18 +0400

    feat: add tracks and image

---
+++ audio-player/js/tracks.js
@@ -0,0 +1,5 @@
+export const tracks = [
+  { id: 1, group: 'xxxcharacter', name: 'MONTAGEM CORAL 1.0 (STADIUM)' },
+  { id: 2, group: 'NRD1', name: 'All Good Things (Come to an End)' },
+  { id: 3, group: 'DR MØB, ZODIVK', name: 'Ainsi Bas La Vida Funk' },
+];
